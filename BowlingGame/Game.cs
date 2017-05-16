fe3ff5f16e7a7066acdb6b5d597dddbf02826008
qualtrax\dallada <dallada@qualtrax.com>
2017-05-16 10:20:48 -0400

    Finished Bowling Score Solution

---
+++ BowlingGame/Game.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace BowlingGame
+{
+    public class Game
+    {
+        public Int32 GetScore(Int32[] rolls)
+        {
+            var score = 0;
+            var rollIndex = 0;
+
+            for (var frame = 1; frame < 10; frame++)
+            {
+                if (IsStrike(rolls[rollIndex]))
+                {
+                    score += GetPinsToAddForStrike(rolls, rollIndex);
+                    rollIndex++;
+                }
+                else if (IsSpare(rolls[rollIndex], rolls[rollIndex + 1]))
+                {
+                    score += GetPinsToAddForSpare(rolls, rollIndex);
+                    rollIndex += 2;
+                }
+                else
+                {
+                    score += GetPinsForFrame(rolls, rollIndex);
+                    rollIndex += 2;
+                }
+            }
+
+            score += GetTenthFramePins(rolls, rollIndex);
+           
+            return score;
+        }
+
+        private static Boolean IsSpare(Int32 firstThrow, Int32 secondThrow)
+        {
+            return firstThrow + secondThrow == 10 && firstThrow != 10;
+        }
+
+        private static Boolean IsStrike(Int32 firstThrow)
+        {
+            return firstThrow == 10;
+        }
+        
+        private static Int32 GetPinsToAddForStrike(Int32[] rolls, Int32 rollIndex)
+        {
+            return 10 + rolls[rollIndex + 1] + rolls[rollIndex + 2];
+        }
+
+        private static Int32 GetPinsToAddForSpare(Int32[] rolls, Int32 rollIndex)
+        {
+            return 10 + rolls[rollIndex + 2];
+        }
+
+        private static Int32 GetPinsForFrame(Int32[] rolls, Int32 rollIndex)
+        {
+            return rolls[rollIndex] + rolls[rollIndex + 1];
+        }
+
+        private static Int32 GetTenthFramePins(Int32[] rolls, Int32 rollIndex)
+        {
+            var tenthFrameScore = 0;
+
+            for (var i = rollIndex; i < rolls.Length; i++)
+                tenthFrameScore += rolls[i];
+
+            return tenthFrameScore;
+        }
+    }
+
+}
---
+++ BowlingGameTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BowlingGameTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BowlingGameTests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5341c4d4-d8ff-4891-b908-58c196b7f023")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
